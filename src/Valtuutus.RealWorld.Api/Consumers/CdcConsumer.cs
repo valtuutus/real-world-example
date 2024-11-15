475bca80c9e2ffeb8483232bb166ab965879d2a5
Yan Pitangui <yanpitangui@gmail.com>
2024-11-15 00:20:51 -0300

    UP: Message envelope

--- src/Valtuutus.RealWorld.Api/Consumers/CdcConsumer.cs
+++ src/Valtuutus.RealWorld.Api/Consumers/CdcConsumer.cs
@@ -1,18 +1,46 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using MassTransit;
 
 namespace Valtuutus.RealWorld.Api.Consumers;
 
-public class CdcConsumer : IConsumer<Message>
+public class CdcConsumer : IConsumer<Batch<CdcEnvelope>>
 {
-    public Task Consume(ConsumeContext<Message> context)
+    private readonly ILogger<CdcConsumer> _logger;
+
+    public CdcConsumer(ILogger<CdcConsumer> logger)
     {
-        throw new NotImplementedException();
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<Batch<CdcEnvelope>> context)
+    {
+        _logger.LogInformation("CDC consumer received batch of {num}", context.Message.Length);
+        foreach (var ctx  in context.Message)
+        {
+            _logger.LogInformation("CDC consumer received {@context}", ctx.Message);
+        }
+        return Task.CompletedTask;
     }
 }
 
-public record Message
+public record CdcEnvelope
 {
-    public JsonDocument Payload { get; init; }
-    
+    public CdcPayload Payload { get; set; }
+}
+
+public record CdcPayload
+{
+    public JsonDocument Before { get; set; }
+    public JsonDocument After { get; set; }
+    public CdcSource Source { get; set; }
+    // TODO: this should probably be an enum
+    [JsonPropertyName("op")]
+    public string Operation { get; set; }
+}
+
+public record CdcSource
+{
+    public string Schema { get; set; }
+    public string Table { get; set; }
 }
